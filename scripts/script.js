795771afb35c2d62243b45d6658c4a4fa6149829
Luke Moats <149527943+LMoats11@users.noreply.github.com>
2024-10-19 18:52:47 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 body {
-    background-image: url('https://www.google.com/imgres?q=mt%20rainier&imgurl=https%3A%2F%2Fwww.visittheusa.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fhero_l%2Fpublic%2Fimages%2Fhero_media_image%2F2016-10%2FMt%2520Rainier.jpg%3Fh%3D4687dff9%26itok%3DGMZdmHDx&imgrefurl=https%3A%2F%2Fwww.visittheusa.com%2Fdestination%2Fmount-rainier-national-park&docid=sqLwkAW_4tWkWM&tbnid=BOWXspOa3pzfUM&vet=12ahUKEwjp8MHsz5uJAxV9RTABHS5WOpgQM3oECGsQAA..i&w=1280&h=700&hcb=2&ved=2ahUKEwjp8MHsz5uJAxV9RTABHS5WOpgQM3oECGsQAA');
+    background-image: url('https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_807,q_75,w_1200/v1/clients/tacoma/Visit_Tacoma_PierceCount_Mt_Rainier_6c4a5f5c-a92d-46d7-9d2b-978c711ca504.jpg')
     background-size: cover; 
     background-repeat: no-repeat; 
     background-attachment: fixed
