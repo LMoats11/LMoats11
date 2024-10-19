e8234b0ce6c35e06df0ded636b21d104c988c37c
Luke Moats <149527943+LMoats11@users.noreply.github.com>
2024-10-19 17:16:52 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+console.log("Page Loaded Successfully");
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
