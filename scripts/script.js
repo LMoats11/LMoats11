9869b982eeb9d737896f5f8b54e70f7f2c6394c7
Luke Moats <149527943+LMoats11@users.noreply.github.com>
2024-10-19 19:11:29 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 body {
-    background-image: url('images/MtRain.jpg');
+    background-image: url('images/MtRain.png');
     background-size: cover; 
     background-repeat: no-repeat; 
-    background-attachment: fixed
-    background-position: center
-    font-family: Robotica;
+    background-attachment: fixed;
+    background-position: center;
+    font-family: 'Roboto', sans-serif;
 }
